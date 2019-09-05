7b2293e6e6a77c39b2dbdd3069f255fe1411bc21
Richard Zou <zou3519@gmail.com>
2019-09-05 14:20:29 -0700

    Remove more BUILD_NAMEDTENSOR macros, guard mobile builds
    Attempting to remove more BUILD_NAMEDTENSOR macros while being careful
    to NOT introduce a dependency on interned_strings.h for certain mobile
    builds.
    
    This is because a dependency on interned_strings.h leads to a
    significant code size increase.
    
    Test Plan
    - [namedtensor ci]
    
    ghstack-source-id: 5614c1896fe25bf92b341a7bde84b17b295c59f1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/25727

--- aten/src/ATen/core/Dimname.h
+++ aten/src/ATen/core/Dimname.h
@@ -1,21 +1,38 @@
 #pragma once
-#ifdef BUILD_NAMEDTENSOR
+
+#if !defined(CAFFE2_IS_XPLAT_BUILD) && (!defined(C10_MOBILE) || defined(FEATURE_TORCH_MOBILE))
 #include <ATen/core/interned_strings.h>
+#endif
+
 #include <c10/util/ArrayRef.h>
 #include <c10/util/Optional.h>
 #include <ostream>
 
+#ifdef BUILD_NAMEDTENSOR
 namespace at {
 
 enum class NameType: uint8_t { NORMAL, WILDCARD, TAGGED };
 
+// If we're building for build-size sensitive platforms, such as when
+// CAFFE2_IS_XPLAT_BUILD is turned on or C10_MOBILE standalone, then
+// we wish to avoid adding an extra dependency on ATen/core/interned_strings.h.
+// In particular, register_symbols.cpp (which is required by interned_strings.h)
+// adds around 6KiB on android.
+//
+// The solution to avoid the dependency is to use std::string as a fallback.
+#if !defined(CAFFE2_IS_XPLAT_BUILD) && (!defined(C10_MOBILE) || defined(FEATURE_TORCH_MOBILE))
+typedef Symbol InternedString;
+#else
+typedef std::string InternedString;
+#endif
+
 struct CAFFE2_API Dimname {
-  static Dimname fromSymbol(Symbol name);
+  static Dimname fromSymbol(InternedString name);
   static Dimname wildcard();
 
   NameType type() const { return type_; }
-  Symbol full_name() const { return full_name_; }
-  Symbol untagged_name() const { return untagged_name_; }
+  InternedString full_name() const { return full_name_; }
+  InternedString untagged_name() const { return untagged_name_; }
 
   bool can_refer_to(const Dimname& other) const;
 
@@ -24,9 +41,9 @@ struct CAFFE2_API Dimname {
   bool is_tagged() const { return type_ == NameType::TAGGED; }
 
  private:
-  Dimname(Symbol name)
+  Dimname(InternedString name)
     : untagged_name_(name), full_name_(name), type_(NameType::NORMAL) {}
-  Dimname(NameType type, Symbol full_name, Symbol untagged_name)
+  Dimname(NameType type, InternedString full_name, InternedString untagged_name)
     : untagged_name_(untagged_name), full_name_(full_name), type_(type) {}
 
   // [Dimname Terminology]
@@ -35,15 +52,14 @@ struct CAFFE2_API Dimname {
   // - "C.in" is the "full name"
   // - "C" is the "untagged name"
   // - "in" is the "tag"
-  Symbol untagged_name_;
-  Symbol full_name_;
+  InternedString untagged_name_;
+  InternedString full_name_;
   NameType type_;
   // Will need more fields for other special name types.
 };
 
 using DimnameList = c10::ArrayRef<Dimname>;
 
-static Symbol kWildcard = Symbol::dimname("*");
 bool CAFFE2_API is_valid_identifier(const std::string& name);
 
 CAFFE2_API c10::optional<Dimname> unify(Dimname dimname, Dimname other);
